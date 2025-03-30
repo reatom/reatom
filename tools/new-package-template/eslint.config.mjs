8a62e1091c5d9b152c0d9db2173c28cf8f809da4
vwency <duckdodo666@gmail.com>
2025-03-30 20:15:54 +0600

    chore(eslint9 mjs setup): new-package-template with sorting imports eslint plugin

---
+++ tools/new-package-template/eslint.config.mjs
@@ -0,0 +1,52 @@
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsEslintParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+        sourceType: 'module',
+        ecmaVersion: 2022,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      // Import sorting
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+  },
+  {
+    ignores: ['node_modules', 
+        'dist', 
+        'README.md', 
+        'CHANGELOG.md', 
+        '*.mjs', 
+        '*.config.js',
+        'build',
+        'node_modules',
+        '*.d.ts',
+      ],
+  },
+];
