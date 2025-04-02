d22aab93caa87927fdb01b8b4340a477a62932d1
vwency <duckdodo666@gmail.com>
2025-04-02 12:21:45 +0600

    chore: remove no-console & interface-prefix rules

--- tools/new-package-template/eslint.config.mjs
+++ tools/new-package-template/eslint.config.mjs
@@ -22,10 +22,8 @@ export default [
       "simple-import-sort/exports": "error",
       "simple-import-sort/imports": "error",
 
-      'no-console': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['error'],
     },
