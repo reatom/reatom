33ceba8431aebbbac403e9d76c76058a6f3b6b3b
Kaifaty <andry.diego2011@gmail.com>
2025-04-04 09:37:44 +0300

    feat: add render count element

--- packages/npm-lit/demo/index.ts
+++ packages/npm-lit/demo/index.ts
@@ -1,24 +1,28 @@
-import { atom } from '@reatom/core'
+import { atom, peek } from '@reatom/core'
 import { ReatomElement, html } from '../src'
 
 const timer = atom(0)
 const countAtom = atom(0)
+const renderCount = atom(0)
+
 const increment = atom(() => {
   countAtom(countAtom() + 1)
 })
+
 setInterval(() => {
   timer(timer() + 1)
-}, 5_000)
+}, 1_000)
 
-const renderCount = atom(0)
+class RenderCountElement extends ReatomElement {
+  protected renderContent() {
+    return html`<div>Render count: ${renderCount}</div>`
+  }
+}
 
 class CounterElement extends ReatomElement {
   static properties = { innerCount: { type: Number, state: true } }
   declare innerCount: number
 
-  private handleClick = () => {
-    increment()
-  }
   private handleClick2 = () => {
     this.innerCount++
   }
@@ -30,7 +34,10 @@ class CounterElement extends ReatomElement {
   }
 
   protected renderContent() {
-    console.log('renderContent')
+    const v = peek(renderCount)
+    renderCount(v + 1)
+
+    console.log('renderContent', v)
     return html`
       <div>
         <h1>Timer: ${timer}</h1>
@@ -40,10 +47,11 @@ class CounterElement extends ReatomElement {
         <button @click=${this.handleClick2}>
           Increment LitElement Reactivity
         </button>
-        <button @click=${this.handleClick}>Increment Reatom Reactivity</button>
+        <button @click=${() => increment()}>Increment Reatom Reactivity</button>
       </div>
     `
   }
 }
 
 customElements.define('counter-element', CounterElement)
+customElements.define('render-count', RenderCountElement)
