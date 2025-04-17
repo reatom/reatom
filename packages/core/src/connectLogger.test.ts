32a930e9c9fdd8787ba47f99f45c6e67f458462c
artalar <artalar@ya.ru>
2025-04-17 22:34:17 +0300

    chore: add logger tests

--- packages/core/src/connectLogger.test.ts
+++ packages/core/src/connectLogger.test.ts
@@ -1,9 +1,9 @@
 import { expect, test } from 'test'
-import { atom, computed } from './core'
+import { atom, computed, notify } from './core'
 import { getStackTrace } from './connectLogger'
 import { sleep, wrap } from '.'
 
-test('log deps graph', async () => {
+test('calc deps graph', async () => {
   // Create atoms for a counter feature
   const counter = atom(0, 'counter').actions((target) => ({
     inc: () => target((s) => s + 1),
@@ -11,34 +11,57 @@ test('log deps graph', async () => {
   const doubled = computed(() => counter() * 2, 'doubled')
   const isEven = computed(() => counter() % 2 === 0, 'isEven')
 
-  let log = ''
+  let stack = ''
   computed(() => {
     return `Count: ${counter()}, Doubled: ${doubled()}, Is Even: ${isEven()}`
   }, 'effect').subscribe(() => {
-    log = getStackTrace()
+    stack = getStackTrace()
   })
 
   // prettier-ignore
-  expect(log).toBe(
+  expect(stack).toBe(
 `
 ─ effect._subscribe ─ effect[#1] ┬─ counter[#2]
                                  │
                                  ├─ doubled[#3] ─ counter[#2]
                                  │
-                                 └─ isEven[#4] ─ counter[#2]
-`.slice(1),
+                                 └─ isEven[#4] ─ counter[#2]`.slice(1),
   )
 
   counter.inc()
   await wrap(sleep())
   // prettier-ignore
-  expect(log).toBe(
+  expect(stack).toBe(
 `
 ─ effect._subscribe ─ effect[#5] ┬─ counter[#6] ─ counter.inc[#7]
                                  │
-                                 ├─ doubled[#8] ─ counter[#6] ─ counter.inc[#7]
+                                 ├─ doubled[#8] ─ counter[#6]
                                  │
-                                 └─ isEven[#9] ─ counter[#6] ─ counter.inc[#7]
-`.slice(1),
+                                 └─ isEven[#9] ─ counter[#6]`.slice(1),
+  )
+})
+
+test('BFS log simplification', () => {
+  const a = atom(0, 'a')
+  const b = computed(() => a(), 'b')
+  const c = computed(() => b(), 'c')
+  const d = computed(() => b() + c(), 'd')
+
+  let stack = ''
+  computed(() => {
+    return d()
+  }, 'effect').subscribe(() => {
+    stack = getStackTrace()
+  })
+
+  a(1)
+  notify()
+
+  expect(stack).toBe(
+    // there should be NO ` ─ a[#20]` after the second `b[#18]`
+    `
+─ effect._subscribe ─ effect[#16] ─ d[#17] ┬─ b[#18] ─ a[#20]
+                                           │
+                                           └─ c[#19] ─ b[#18]`.slice(1),
   )
 })
