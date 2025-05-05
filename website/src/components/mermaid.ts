94bb0986bbae02e22d2dc93c58fe50069113bd79
BANO <me@bano.dev>
2025-05-05 03:14:25 +0200

    chore: remove unused files

--- website/src/components/mermaid.ts
+++
@@ -1,9 +0,0 @@
-import mermaid from 'mermaid'
-
-// TODO integrate mermaid as astro plugin instead
-
-mermaid.initialize({
-  theme: document.documentElement.dataset.theme === 'dark' ? 'dark' : 'pastel',
-})
-
-export { mermaid }
