19134c5cd23b914ee0db4c795a0110951e34f8c6
BANO <me@bano.dev>
2025-05-05 03:19:07 +0200

    fix: use dirs in references

--- website/references.config.ts
+++ website/references.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from './config/integrations/package-reference.ts'
 
 export const references = await defineConfig([
-  '../packages/core',
-  '../packages/jsx',
+  '../packages/core/',
+  '../packages/jsx/',
 ])
