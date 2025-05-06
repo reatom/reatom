3c5157c0944c3a3a5bb3b553f86b5a75c1abd69f
artalar <artalar@ya.ru>
2025-05-06 09:52:36 +0300

    chore: chore

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,6 +3,7 @@ import { defineWorkspace } from 'vitest/config'
 export default defineWorkspace([
   './packages/jsx/vitest.config.ts',
   './packages/react/vitest.config.ts',
+  './packages/preact/vitest.config.ts',
   './packages/core/vitest.config.ts',
   './packages/core/vitest.browser.config.ts',
 ])
