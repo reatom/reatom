47a5569666b0d4a8f4e4d5138e03f77574571f30
artalar <artalar@ya.ru>
2025-05-25 21:08:43 +0300

    chore: remove old packages

--- packages/esm-import-check/src/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest'
-import { atom, createCtx } from '@reatom/framework'
-
-test('test ESM import', () => {
-  const ctx = createCtx()
-  const me = atom('check ✅', 'me')
-
-  expect(ctx.get(me)).toBe('check ✅')
-})
