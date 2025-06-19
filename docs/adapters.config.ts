3fc188bad0b071cdf7d6076a7653d6ec2b4f16bf
BANO <me@bano.dev>
2025-06-19 19:46:43 +0200

    chore: remove old adapters

--- docs/adapters.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from './config/integrations/package-reference'
-
-export const adapters = await defineConfig([
-  '../packages/lit/',
-  '../packages/preact/',
-  '../packages/react/',
-  '../packages/vue/',
-])
