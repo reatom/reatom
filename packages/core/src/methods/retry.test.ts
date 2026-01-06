b715e15a36606ea6f5ccff971d8d374bd3cf1c1d
Aleksei Gurianov <gurianov@gmail.com>
2026-01-06 10:24:37 +0300

    test(retry): add test for dependent computed recalculation

--- packages/core/src/methods/retry.test.ts
+++ packages/core/src/methods/retry.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from 'test'
 
 import { withAsync } from '../async'
-import { action } from '../core'
+import { action, computed } from '../core'
 import { retryComputed } from './retry'
+import { wrap } from './wrap'
 
 test('action retry disabled by default', async () => {
   const name = 'actionRetryDisabled'
@@ -11,4 +12,40 @@ test('action retry disabled by default', async () => {
   )
 
   expect(() => retryComputed(fetch)).toThrow('Only reactive atoms can be reset')
+})
+
+test('retryComputed should recalculate dependent computeds', async () => {
+  const computedA = computed(() => Math.random(), 'computedA')
+  const computedB = computed(() => computedA() * 10, 'computedB')
+
+  // Subscribe to both to track them
+  const valuesA: number[] = []
+  const valuesB: number[] = []
+
+  computedA.subscribe((v) => valuesA.push(v))
+  computedB.subscribe((v) => valuesB.push(v))
+
+  // Wait for initial subscriptions to settle
+  await wrap(Promise.resolve())
+
+  const initialA = valuesA[0]!
+  const initialB = valuesB[0]!
+
+  expect(initialB).toBe(initialA * 10)
+  expect(valuesA.length).toBe(1)
+  expect(valuesB.length).toBe(1)
+
+  // Retry computedA - should recalculate both A and B
+  retryComputed(computedA)
+
+  // Wait for notifications to propagate
+  await wrap(Promise.resolve())
+
+  const newA = valuesA[1]!
+  const newB = valuesB[1]!
+
+  expect(valuesA.length).toBe(2)
+  expect(valuesB.length).toBe(2) // This is the bug - computedB is not notified
+  expect(newA).not.toBe(initialA) // Should be a new random value
+  expect(newB).toBe(newA * 10) // computedB should have recalculated
 })
