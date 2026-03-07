455717d0a89d8538c21e6c959894f5016f5b5e43
artalar <artalar@ya.ru>
2026-03-07 16:33:34 +0300

    test: fix deopt

--- packages/core/deopt.js
+++ packages/core/deopt.js
@@ -1,23 +1,23 @@
 // Test code for Deopt Explorer
 // run `dexnode deopt.js`
 
-import { atom, notify } from './build/index.mjs'
+import { atom, computed, notify } from './build/index.js'
 
 const name = 'diamonds'
 const a1 = atom(0, `${name}.a1`)
-const a2 = atom(() => {
-  return a1() + a1() - a1()
+const a2 = computed(() => {
+  return a1() + a1() + a1()
 }, `${name}.a2`)
-const a3 = atom(() => {
+const a3 = computed(() => {
   return a1()
 }, `${name}.a3`)
-const a4 = atom(() => {
+const a4 = computed(() => {
   return a2() + a3()
 }, `${name}.a4`)
-const a5 = atom(() => {
+const a5 = computed(() => {
   return a2() + a3()
 }, `${name}.a5`)
-const a6 = atom(() => {
+const a6 = computed(() => {
   return a4() + a5()
 }, `${name}.a6`)
 
@@ -25,6 +25,10 @@ a6.subscribe()
 
 let i = 1000
 while (i--) {
-  a1(i)
+  a1.set(i)
   notify()
 }
+
+// TODO why not end automatically?
+// eslint-disable-next-line no-undef
+process.exit(0)
