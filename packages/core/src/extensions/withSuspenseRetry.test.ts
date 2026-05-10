e6bb1f79a99822c56b72e5123e09a92c23228b69
artalar <artalar@ya.ru>
2026-05-10 22:30:13 +0300

    test(extensions): some

--- packages/core/src/extensions/withSuspenseRetry.test.ts
+++ packages/core/src/extensions/withSuspenseRetry.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from 'test'
 
+import { withAsyncData } from '../async/withAsyncData'
 import { action, atom } from '../core'
-import { wrap } from '../methods'
-import { sleep } from '../utils'
+import { abortVar, wrap } from '../methods'
+import { isAbort, noop, sleep } from '../utils'
 import { withSuspenseInit } from './withSuspense'
 import { withSuspenseRetry } from './withSuspenseRetry'
 
@@ -18,4 +19,32 @@ test('suspense retry', async () => {
   }).extend(withSuspenseRetry())
 
   expect(await wrap(act())).toBe(1)
+})
+
+test('suspense retry with async data aborts while suspended', async () => {
+  const deferred = Promise.withResolvers<number>()
+  const suspenseAtom = atom(async () => deferred.promise).extend(
+    withSuspenseInit(),
+  )
+
+  const errors: Array<unknown> = []
+  const act = action(async () => {
+    abortVar.subscribe((error) => {
+      errors.push(error)
+    })
+    suspenseAtom()
+
+    await wrap(sleep())
+  }).extend(withSuspenseRetry(), withAsyncData())
+
+  act().catch(noop)
+
+  await wrap(Promise.resolve())
+  deferred.resolve(1)
+  await wrap(Promise.resolve())
+  act.abort('manual')
+
+  expect(errors).toHaveLength(1)
+  expect(isAbort(errors[0])).toBe(true)
+
 })
