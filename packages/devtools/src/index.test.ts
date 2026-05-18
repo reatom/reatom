1a5da170dcc66f997613f3c4644b4491a0a653e0
artalar <artalar@ya.ru>
2026-05-18 12:35:03 +0300

    chore: remove devtools

--- packages/devtools/src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest'
-import { createTestCtx } from '@reatom/testing'
-
-test('stub', () => {
-  const ctx = createTestCtx()
-
-  // expect.fail('No tests!')
-  expect(true).toBe(true) // Placeholder for an actual test
-})
